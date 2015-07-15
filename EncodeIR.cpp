be6dc4dc04259b602b18a076ed72ba5d0cb17ab3
probonopd <probono@puredarwin.org>
2015-07-16 00:46:59 +0200

    Remove header since this file was not part of original MakeHex

--- EncodeIR.cpp
+++ EncodeIR.cpp
@@ -1,22 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-// MakeHex version 4  Copyright 2004 John S. Fine
-//
-// You may use, copy, modify and/or distribute this program for private or
-// commercial use provided that:
-//
-// 1)  You do not hold me responsible for any damage or negative consequences
-// resulting from those activities.
-//
-// 2)  You include this copyright notice and disclaimer in any copy of any part
-// or all of this program.
-//
-// I do not provide any warranty of the correctness, safety, or suitibility of
-// this program for any purpose.
-//
-// If you do not agree to these conditions, you have no permission to use, copy,
-// modify or distribute this program.
-///////////////////////////////////////////////////////////////////////////////
-
 #include "stdafx.h"
 #include "irp.h"
 
